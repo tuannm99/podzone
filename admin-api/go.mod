4dd40a9e63e02dd0bb867edeec55483edf9c082a
tuannm99 <gm.tuannm99@gmail.com>
2025-02-11 22:45:50 +0700

    migrate

---
+++ admin-api/go.mod
@@ -0,0 +1,3 @@
+module github.com/tuannm99/podzone/admin-api
+
+go 1.23.4
---
+++ admin-api/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("change to company tech. -> go and angular")
+}
