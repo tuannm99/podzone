5c424edcb71473efec2e0af918d2aa93abfc1994
tuannm99 <gm.tuannm99@gmail.com>
2025-10-07 21:26:56 +0700

    update: mongo version

--- internal/backoffice/module.go
+++ internal/backoffice/module.go
@@ -6,6 +6,7 @@ import (
 	"github.com/tuannm99/podzone/internal/backoffice/controller/graphql/resolver"
 	interactor "github.com/tuannm99/podzone/internal/backoffice/domain"
 	"github.com/tuannm99/podzone/internal/backoffice/domain/inputport"
+	"github.com/tuannm99/podzone/internal/backoffice/domain/outputport"
 	"github.com/tuannm99/podzone/internal/backoffice/infrastructure/repository"
 )
 
@@ -13,13 +14,10 @@ import (
 var Module = fx.Options(
 	fx.Provide(
 		// --- Infrastructure layer ---
-		repository.NewStoreRepository,
+		fx.Annotate(repository.NewStoreRepository, fx.As(new(outputport.StoreRepository))),
 
 		// --- Domain layer ---
-		fx.Annotate(
-			interactor.NewStoreInteractor,
-			fx.As(new(inputport.StoreUsecase)),
-		),
+		fx.Annotate(interactor.NewStoreInteractor, fx.As(new(inputport.StoreUsecase))),
 
 		// --- GraphQL resolver root ---
 		resolver.NewResolver,
